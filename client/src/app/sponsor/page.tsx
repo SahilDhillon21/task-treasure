e19a10c52b8a0331a4fe18f37b0cf9a2342b36bf
SahilDhillon21 <sodhillon_b21@it.vjti.ac.in>
2024-08-24 16:52:17 +0530

    commit

--- client/src/app/sponsor/page.tsx
+++ client/src/app/sponsor/page.tsx
@@ -1,7 +1,23 @@
-import { useSession } from 'next-auth/react'
+"use client"
 
 const SponsorPage = () => {
-    const { data: session } = useSession();
+  return (
+    <>
+      
 
-    
-}
+      <section className="pb-[120px] pt-28">
+        <div className="container ">
+
+
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <h1>Hi sponsor</h1>
+            </div>
+          </div>
+        </div>
+      </section >
+    </>
+  );
+};
+
+export default SponsorPage;
